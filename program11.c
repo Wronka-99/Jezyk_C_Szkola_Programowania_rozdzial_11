829854f2bd19bb23fbadd39ae41331b316d0fd55
Bartosz Wroński <bartek-wronski7@wp.pl>
2021-11-15 13:53:32 +0100

    commit

--- program11.c
+++ program11.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+*Do pobrania ciągu znaków służą trzy funkcje: gets, scnaf oraz fgets.
+*Gets pobiera znaki aż do pojawienia się znaku nowej linii. 
+*Scanf pobiera tylko pojedyncze słowo do spacji.
+*
+*
+*
+*
+*/
+
+#define SIZE 16
 
 int main()
 {
-
-
+    char slowa[SIZE];
+    printf("Podaj swoje wyrazenie.\n");
+    gets(slowa);
+    printf("Wypisanie łańcucha na dwa sposoby:\n\n");
+    puts(slowa);
+    printf("%s\n", slowa);
+    printf("Koniec programu.\n");
 
     return 0;
 }
